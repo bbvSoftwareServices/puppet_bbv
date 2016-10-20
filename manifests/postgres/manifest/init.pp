0189abe5d9d8f9c9311e1302aeed70d7f3d59b61
Gerald Mixa <Gerald.Mixa@bbv.eu>
2016-10-20 15:16:00 +0200

    Postgres 9.6 installieren und konfigurieren!

--- manifests/postgres/manifest/init.pp
+++ manifests/postgres/manifest/init.pp
@@ -7,13 +7,8 @@ class { 'postgresql::server':
   ip_mask_deny_postgres_user => '0.0.0.0/32',
   ip_mask_allow_all_users    => '0.0.0.0/0',
   listen_addresses           => '*',
-  postgres_password          => 'postgres',
+  postgres_password          => 'postgres',}
 
-  postgresql::server::db { 'bbvtestdatabase':
-    user  => 'bbvuser',
-    password => postgresql_password('bbvuser','bbvpassword')
-  }
-} ->
 class { 'postgresql::server::contrib':
   package_ensure => 'present',
 }
