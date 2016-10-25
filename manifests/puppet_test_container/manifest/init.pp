feb24a66f54be397773d7f3d117b7892b4862ffb
Gerald Mixa <Gerald.Mixa@bbv.eu>
2016-10-25 16:57:14 +0200

    Fix Dependencies

--- manifests/puppet_test_container/manifest/init.pp
+++ manifests/puppet_test_container/manifest/init.pp
@@ -21,13 +21,13 @@ class puppet_test_container {
   file { '/etc/init.d/puppettestcontainer':
     ensure => 'link',
     target => '/opt/bbv/puppettestcontainer-0.0.1.jar',
-    before => Service['puppettestcontainer'],
+    # before => Service['puppettestcontainer'],
   }
 
-  service { 'puppettestcontainer':
-    ensure => running,
-    start => 'service pupettestcontainer start',
-    stop =>  'service puppettestcontainer stop'
-  }
+  #service { 'puppettestcontainer':
+  #  ensure => running,
+  #  start => 'service pupettestcontainer start',
+  #  stop =>  'service puppettestcontainer stop'
+ # }
 
 }
